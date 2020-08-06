130379d3fa71a7d5c79d23794a4723a433c6415a
VaishnaviPawar1297 <vaishnavip1012@gmail.com>
2020-08-06 17:59:20 +0530

    Flipping a Coin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 #Welcome to Flip Coin Simulator
+#Coin Flip.....................................................................................
+toss=$((RANDOM%2))
+if [ $toss -eq 1 ]
+then
+	echo "win"
+else
+	echo "loss"
+fi
